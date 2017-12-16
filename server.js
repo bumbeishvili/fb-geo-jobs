9958ab6c16dde7a8c7b44878cea46b74cbdaee49
Dato <davit.bumbeishvili@gmail.com>
2017-12-16 19:09:02 +0400

    replace  3000 with heroku port

--- server.js
+++ server.js
@@ -44,5 +44,5 @@ app.get('/log', (req, res, next) => {
 })
 
 
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+var port = process.env.PORT || 3000;
+app.listen(port, () => console.log('Example app listening on port 3000!'))
