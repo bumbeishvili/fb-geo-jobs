09f7f0d0048f5c1358250a9e463e8298eabfded2
David  Bumbeishvili <bumbeishvili@users.noreply.github.com>
2017-12-25 18:46:48 +0400

    Add currency title

--- src/scraper.js
+++ src/scraper.js
@@ -304,7 +304,7 @@ function myJobsCrawler(category, callback) {
         createdAt: new Date(),
         pos: d.vac_title,
         company: d.comp_title,
-        salary: d.salary ? d.salary : null,
+        salary: d.salary ? d.salary+" ლარი" : null,
         scrapedForSalary: d.salary ? true : null,
         link: category.vacancyLinkTemplate + d.id,
       }
